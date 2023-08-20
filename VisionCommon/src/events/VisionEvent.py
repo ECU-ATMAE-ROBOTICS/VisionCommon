a63f80f37f3eee4c7a9c53afc7c95ad734a375cf
JarredTD <jarred.desrosiers@yahoo.com>
2023-08-20 00:46:40 -0700

    Remove event classes

--- VisionCommon/src/events/VisionEvent.py
+++
@@ -1,8 +0,0 @@
-# Built-in
-from typing import Optional
-
-
-class VisionEvent:
-    def __init__(self, eventType: str, data: Optional[any] = None) -> None:
-        self.eventType = eventType
-        self.data = data
